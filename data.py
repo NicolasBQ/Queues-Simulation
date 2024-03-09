b7e298ad34f1bfff7b8c17a39d3f8fa967e53eea
Nicolas Baracaldo <nicolasbquintero@gmail.com>
2024-03-09 13:04:20 -0500

    Inverse Exponential and Inverse Normal

--- data.py
+++ data.py
@@ -1,9 +1,40 @@
 import pseudorandom
+from scipy.stats import norm, expon
 
 def queue_data(lambda_value, miu_value, desv_value, n_value):
-    random_exponential = pseudorandom.random_numbers(n_value)
-    random_normal = pseudorandom.random_numbers(n_value)
+    # random_exponential = pseudorandom.random_numbers(n_value)
+    # random_normal = pseudorandom.random_numbers(n_value)
+    random_exponential = [
+        0.103545,
+        0.85029,
+        0.55093,
+        0.75017
+    ]
 
-    print(random_exponential)
-    print(random_normal)
+    random_normal = [
+        0.58093,
+        0.7437,
+        0.25094,
+        0.10345
+    ]
+
+    arrival = arrival_exp(random_exponential, lambda_value)
+    attention = attention_norm(random_normal, miu_value, desv_value)
+    print(arrival)
+    print(attention)
+
+
+def arrival_exp(random_exponential, lambda_value):
+    arrival = []
+    # Calcular la inversa exponencial
+    arrival = expon.ppf(random_exponential, scale=1 / lambda_value)
+    
+    return arrival
+
+def attention_norm(random_normal, miu_value, desv_value):
+    attention = []
+    # Calcular la inversa normal
+    attention = norm.ppf(random_normal, loc=miu_value, scale=desv_value)
+    return attention
+
 
